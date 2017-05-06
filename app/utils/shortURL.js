26f44e55178227ad5208ef854e8e98927922391f
ABDULLAHPC\ssabd <ssabdullah_1992@hotmail.com>
2017-05-06 16:04:52 +1000

    JavaScript for Short URL

---
+++ app/utils/shortURL.js
@@ -0,0 +1,28 @@
+function myFuncion()
+{
+	var request = require('request');
+	var Sync = require('sync');
+
+	// Set the headers
+	var headers = {
+	    'Content-Type': 'application/json'
+	}
+
+	// Configure the request
+	var options = {
+	    url: 'https://www.googleapis.com/urlshortener/v1/url?key=AIzaSyDSzPlQBcgLbwdRTlmqWQEJbaJcMqNmtV0',
+	    method: 'POST',
+	    headers: headers,
+	    json: {'longUrl': 'https://cloud.google.com/natural-language/docs/basics#entity-analysis-response'}
+	}
+
+
+	request(options, function (error, response, body) {
+	    if (!error && response.statusCode == 200) {
+	        // Print out the response body
+			console.log(body.id);   
+	    }
+	});
+}
+
+myFuncion();
