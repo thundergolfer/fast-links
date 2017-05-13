d87c9a4a8ff77aa4f6a7f2e18760672997427c6b
Jonathon Belotti <jbelotti@zendesk.com>
2017-05-13 19:33:49 +1000

    setup new testing folder

---
+++ test/extension/entity_recognition.spec.js
@@ -0,0 +1,20 @@
+var assert = require('assert');
+
+var nlpAnalyser = require('../../chrome/extension/entity_recognition');
+
+describe('EXAMPLE TEST ONLY', function () {
+    describe('#indexOf', function () {
+        it('should return -1 when the substring is not present', function () {
+            var myString = 'test';
+
+            assert.equal(-1, myString.indexOf('x'));
+            assert.equal(-1, myString.indexOf('y'));
+        });
+    });
+});
+
+describe('entity_recognition', function() {
+  it('should return true because I dont have a test yet', function() {
+    assert.equal(1, 1);
+  });
+})
